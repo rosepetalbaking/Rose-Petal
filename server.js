3b41bc00c7ebd979ea7fd7de5ecd3be0c742f460
rosepetalbaking <zarazara061216@gmail.com>
2025-12-27 03:10:57 -0500

    Delete server.js

--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-
-app.use(express.static(__dirname));
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log("Sugar Petal running");
-});
